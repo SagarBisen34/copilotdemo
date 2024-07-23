ceea396e3d2eef2fe51d34655efa6cbb38a103a5
Sagar Bisen <sagar.bisen@aderant.com>
2024-07-23 12:04:46 +0530

    Initial commit removed absolute path

--- src/test/java/TestRunner/junittTestRunner.java
+++ src/test/java/TestRunner/junittTestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-features = {"D:/AI-CopilotDemo/aicopilotdemo/src/test/resources/features/login.feature"},
+features = {"src/test/resources/features/login.feature"},
         glue ={"stepdefinitions"},
         dryRun = false,
         monochrome = true,
